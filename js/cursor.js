277d6de72023b99c183d63b73ba33a53453e096f
Rene Hampölz <rene@hampoelz.net>
2021-10-22 17:20:56 +0200

    chore: :lipstick: Optimize cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,40 +1,26 @@
-CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
-    this.beginPath();
-    this.moveTo(x + r, y);
-    this.arcTo(x + w, y, x + w, y + h, r);
-    this.arcTo(x + w, y + h, x, y + h, r);
-    this.arcTo(x, y + h, x, y, r);
-    this.arcTo(x, y, x + w, y, r);
-    this.closePath();
-    return this;
-}
-
-function colorCursor(stylesheet, color) {
-    let cursor = document.createElement('canvas'),
-        ctx = cursor.getContext('2d');
-
-    cursor.width = 10;
-    cursor.height = 10;
-
-    ctx.fillStyle = color;
-    ctx.roundRect(0, 0, cursor.width, cursor.height, 20).fill();
-
-    //document.body.style.setProperty('cursor', )
-
-    const rules = stylesheet.cssRules;
-    rules[0].style.setProperty('cursor', 'url(' + cursor.toDataURL() + '), auto', 'important')
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+function removeMousePointer() {
     const stylesheet = new CSSStyleSheet();
     stylesheet.insertRule('* { cursor: auto; }', 0);
     document.adoptedStyleSheets = [stylesheet];
 
+    let cursor = document.createElement('canvas'),
+        ctx = cursor.getContext('2d');
+
+    cursor.width = 1;
+    cursor.height = 1;
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.01)';
+    ctx.fillRect(0, 0, cursor.width, cursor.height);
+
+    const rules = stylesheet.cssRules;
+    rules[0].style.setProperty('cursor', 'url(' + cursor.toDataURL() + '), auto', 'important');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let mousePointer = document.getElementById('pointer');
     let mouseCursor = document.getElementById('cursor');
-    
-    colorCursor(stylesheet, window.getComputedStyle(mouseCursor).borderColor);
+
+    removeMousePointer();
 
     let lock = false;
     let currentPos = { x: 0, y: 0 };
@@ -47,15 +33,18 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPos.x += (aimPos.x - currentPos.x) * mul;
         currentPos.y += (aimPos.y - currentPos.y) * mul;
 
-        if (aimPos.y != -1) mouseCursor.style.top = currentPos.y + "px";
-        if (aimPos.x != -1) mouseCursor.style.left = currentPos.x + "px";
+        if (aimPos.y != -1) mouseCursor.style.top = currentPos.y - parseFloat(window.getComputedStyle(mouseCursor).height) / 2 - parseFloat(window.getComputedStyle(mouseCursor).borderTopWidth) + parseFloat(window.getComputedStyle(mousePointer).height) / 2 + "px";
+        if (aimPos.x != -1) mouseCursor.style.left = currentPos.x - parseFloat(window.getComputedStyle(mouseCursor).width) / 2 - parseFloat(window.getComputedStyle(mouseCursor).borderLeftWidth) + parseFloat(window.getComputedStyle(mousePointer).width) / 2 + "px";
     }
 
     animate();
 
     document.addEventListener('touchstart', () => { lock = true; });
-    
+
     document.addEventListener('mousemove', event => {
+        mousePointer.style.top = event.clientY + "px";
+        mousePointer.style.left = event.clientX + "px";
+
         if (lock) {
             lock = false;
             return;
@@ -71,21 +60,16 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.target.classList.contains('clickable')) {
             mouseCursor.classList.add('click');
             mouseCursor.classList.remove('normal');
-            mouseCursor.classList.remove('pointer');
-            colorCursor(stylesheet, 'rgba(255, 255, 255, 0.1)');
+            mouseCursor.classList.remove('hand');
+        } else if (event.target.classList.contains('hoverable') || event.target.classList.contains('os-scrollbar-handle')) {
+            mouseCursor.classList.add('hand');
+            mouseCursor.classList.remove('click');
+            mouseCursor.classList.remove('normal');
         } else {
-            colorCursor(stylesheet, window.getComputedStyle(mouseCursor).borderColor);
-
-            if (event.target.classList.contains('hoverable') || event.target.classList.contains('os-scrollbar-handle')) {
-                mouseCursor.classList.add('pointer');
-                mouseCursor.classList.remove('click');
-                mouseCursor.classList.remove('normal');
-                
-            } else {
-                mouseCursor.classList.add('normal');
-                mouseCursor.classList.remove('click');
-                mouseCursor.classList.remove('pointer');
-            }
+            mouseCursor.classList.add('normal');
+            mouseCursor.classList.remove('click');
+            mouseCursor.classList.remove('hand');
         }
+
     });
 });
