9e27a36b17b94081c83dd8492c06c1a0f3433c3d
Rene Hampölz <rene@hampoelz.net>
2021-10-22 16:14:40 +0200

    chore: :lipstick: Draw cursor as point & Enhance cursor hover effect

--- js/cursor.js
+++ js/cursor.js
@@ -1,5 +1,40 @@
+CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
+    if (w < 2 * r) r = w / 2;
+    if (h < 2 * r) r = h / 2;
+    this.beginPath();
+    this.moveTo(x + r, y);
+    this.arcTo(x + w, y, x + w, y + h, r);
+    this.arcTo(x + w, y + h, x, y + h, r);
+    this.arcTo(x, y + h, x, y, r);
+    this.arcTo(x, y, x + w, y, r);
+    this.closePath();
+    return this;
+}
+
+function colorCursor(stylesheet, color) {
+    let cursor = document.createElement('canvas'),
+        ctx = cursor.getContext('2d');
+
+    cursor.width = 10;
+    cursor.height = 10;
+
+    ctx.fillStyle = color;
+    ctx.roundRect(0, 0, cursor.width, cursor.height, 20).fill();
+
+    //document.body.style.setProperty('cursor', )
+
+    const rules = stylesheet.cssRules;
+    rules[0].style.setProperty('cursor', 'url(' + cursor.toDataURL() + '), auto', 'important')
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    const stylesheet = new CSSStyleSheet();
+    stylesheet.insertRule('* { cursor: auto; }', 0);
+    document.adoptedStyleSheets = [stylesheet];
+
     let mouseCursor = document.getElementById('cursor');
+    
+    colorCursor(stylesheet, window.getComputedStyle(mouseCursor).borderColor);
 
     let lock = false;
     let currentPos = { x: 0, y: 0 };
@@ -33,13 +68,24 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.addEventListener('mouseover', event => {
-        if (window.getComputedStyle(event.target)['cursor'] == 'pointer' ||
-            event.target.classList.contains('os-scrollbar-handle')) {
-            mouseCursor.classList.add('pointer');
+        if (event.target.classList.contains('clickable')) {
+            mouseCursor.classList.add('click');
             mouseCursor.classList.remove('normal');
-        } else {
-            mouseCursor.classList.add('normal');
             mouseCursor.classList.remove('pointer');
+            colorCursor(stylesheet, 'rgba(255, 255, 255, 0.1)');
+        } else {
+            colorCursor(stylesheet, window.getComputedStyle(mouseCursor).borderColor);
+
+            if (event.target.classList.contains('hoverable') || event.target.classList.contains('os-scrollbar-handle')) {
+                mouseCursor.classList.add('pointer');
+                mouseCursor.classList.remove('click');
+                mouseCursor.classList.remove('normal');
+                
+            } else {
+                mouseCursor.classList.add('normal');
+                mouseCursor.classList.remove('click');
+                mouseCursor.classList.remove('pointer');
+            }
         }
     });
 });
