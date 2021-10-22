d39df9b9d9ebaab69a97a1bb24051f48ec815c59
Rene Hampölz <rene@hampoelz.net>
2021-10-22 10:24:56 +0200

    chore: :lipstick: Replace scrollbar with 'OverlayScrollbars' by kingsora

---
+++ js/scrollbar.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    OverlayScrollbars(document.querySelectorAll('body'), {
+        className: "os-theme-thin-light",
+        touchSupport: false
+    });
+});
