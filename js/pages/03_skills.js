e03d23fbf3b973db51cbc8cf9a10fa32a7fbe2e0
Rene Hampölz <rene@hampoelz.net>
2022-03-09 18:56:59 +0100

    chore(skills): :construction: Add basic structure and style for the timeline

---
+++ js/pages/03_skills.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const createDataScrollAttr = () => document.createAttribute("data-scroll");
+    const createDataScrollStickyAttr = () => document.createAttribute("data-scroll-sticky");
+    const createDataScrollOffsetAttr = () => {
+        let dataScrollOffset = document.createAttribute("data-scroll-offset");
+        dataScrollOffset.value = "-10%, 10%";
+        return dataScrollOffset;
+    };
+
+    const timelines = document.querySelectorAll("#skills .timeline");
+    for (let i = 0; i < timelines.length; i++) {
+        const timeline = timelines[i];
+        const timelineHeaders = timeline.querySelectorAll("section.year h3");
+        for (let j = 0; j < timelineHeaders.length; j++) {
+            const dataScrollTarget = document.createAttribute("data-scroll-target");
+            dataScrollTarget.value = `#skills .timeline:nth-of-type(${i + 1}) section.year:nth-child(${j + 1})`;
+
+            const header = timelineHeaders[j];
+            header.setAttributeNode(createDataScrollAttr());
+            header.setAttributeNode(createDataScrollStickyAttr());
+            header.setAttributeNode(createDataScrollOffsetAttr());
+            header.setAttributeNode(dataScrollTarget);
+        }
+    }
+});
