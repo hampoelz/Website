2295cbe162109285c38ec162aa16b2227f244381
Rene Hampölz <rene@hampoelz.net>
2022-03-09 19:39:35 +0100

    chore(social-media): :fire: Remove parallax effect on social media cards

--- js/pages/02_connect-social.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const socialContainer = document.querySelectorAll("#connect-social .container");
-
-    new MouseParallax([socialContainer], { multiplier: 0.001 });
-});
