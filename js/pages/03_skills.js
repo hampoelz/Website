78cfd6a5567b626a9deb978ca907546156d315fe
Rene Hampölz <rene@hampoelz.net>
2022-07-20 15:43:31 +0200

    chore(skills): :dizzy: Add timeline section animations

--- js/pages/03_skills.js
+++ js/pages/03_skills.js
@@ -11,61 +11,108 @@ document.addEventListener("DOMContentLoaded", () => {
   for (let i = 0; i < timelines.length; i++) {
     const timeline = timelines[i];
 
-    const timelineHeaders = timeline.querySelectorAll("section.year h3");
-    for (let j = 0; j < timelineHeaders.length; j++) {
-      const dataScrollTarget = document.createAttribute("data-scroll-target");
-      dataScrollTarget.value = `#skills .timeline:nth-of-type(${i + 1}) section.year:nth-child(${j + 1})`;
-
-      const header = timelineHeaders[j];
-      header.setAttributeNode(createDataScrollAttr());
-      header.setAttributeNode(createDataScrollStickyAttr());
-      header.setAttributeNode(createDataScrollOffsetAttr());
-      header.setAttributeNode(dataScrollTarget);
-    }
-
+    let offset = 0;
     let pastOffset = 0;
+    
+    let pointPositions = [];
 
     const timelineYears = timeline.querySelectorAll("section.year");
+
     for (let j = 0; j < timelineYears.length; j++) {
-      const section = timelineYears[j];
+      const yearSection = timelineYears[j];
+
+      let sectionOffset = 0;
+
+      const timelineSections = yearSection.querySelectorAll("section");
+      const timelineFutureSections = yearSection.querySelectorAll("section.future:not(.pseudo-future)");
       
-      const timelineFuture = section.querySelectorAll("section.future");
-      if (timelineFuture.length <= 0) pastOffset += section.offsetHeight + parseInt(getComputedStyle(section).marginBottom);
-      else {
-        const timelinePast = section.querySelectorAll("section");
-        for (let k = 0; k < timelinePast.length; k++) {
-          const pastSection = timelinePast[k];
+      for (let k = 0; k < timelineSections.length; k++) {
+        const section = timelineSections[k];
 
-          if (pastSection == timelineFuture[0]) break;
+        const isFirstSection = k == 0;
+        const isLastSection = k == timelineSections.length - 1;
 
-          if (k == 0) {
-            const header = section.querySelector("h3");
+        const isFutureSection = Array.from(timelineFutureSections).includes(section);
+        const isFirstFutureSection = timelineFutureSections[0] == section;
 
-            const headerTop = header.getBoundingClientRect().top;
-            const sectionTop = pastSection.getBoundingClientRect().top;
-            
-            pastOffset += sectionTop - headerTop;
-          }
+        pointPositions.push({ element: section, offset: offset + sectionOffset});
 
-          pastOffset += pastSection.offsetHeight + parseInt(getComputedStyle(pastSection).marginBottom);;
+        if (!isFutureSection) {
+          section.classList.add("future", "pseudo-future");
         }
 
-        break;
+        if (isFirstFutureSection) {
+          pastOffset = offset + sectionOffset;
+        }
+
+        if (isFirstSection) {
+          const yearSectionHeader = yearSection.querySelector("h3");
+
+          const dataScrollTarget = document.createAttribute("data-scroll-target");
+          dataScrollTarget.value = `#skills .timeline:nth-of-type(${i + 1}) section.year:nth-child(${j + 1})`;
+
+          yearSectionHeader.setAttributeNode(createDataScrollAttr());
+          yearSectionHeader.setAttributeNode(createDataScrollStickyAttr());
+          yearSectionHeader.setAttributeNode(createDataScrollOffsetAttr());
+          yearSectionHeader.setAttributeNode(dataScrollTarget);
+
+          const headerTop = yearSectionHeader.getBoundingClientRect().top;
+          const sectionTop = section.getBoundingClientRect().top;
+          sectionOffset += sectionTop - headerTop;
+        }
+
+        sectionOffset += section.offsetHeight + parseFloat(getComputedStyle(section).marginBottom);
+
+        if (isLastSection) {
+          sectionOffset += (yearSection.offsetHeight + parseFloat(getComputedStyle(yearSection).marginBottom) - sectionOffset)
+        }
       }
+
+      offset += sectionOffset;
     }
 
     const pixelPercent = (1 / timeline.offsetHeight) * 100;
     const futureTransPercent = pixelPercent * pastOffset;
     const futureTransPercentVar = `--timeline${i + 1}-futureTransPercent`
 
+    pointPositions = pointPositions.map(point => { return { element: point.element, offset: pixelPercent * point.offset } });
     document.documentElement.style.setProperty(futureTransPercentVar, "0%");
-
+    
     gsap.timeline({
       scrollTrigger: {
         trigger: `#skills .timeline:nth-of-type(${i + 1})`,
         start: "top bottom",
         end: "bottom-=30%",
-        scrub: 1
+        scrub: 1,
+        onUpdate: trigger => {
+          const futureTransPercent = document.documentElement.style.getPropertyValue(futureTransPercentVar);
+
+          if (trigger.direction == 1) {
+            const filteredPoints = pointPositions.filter(position => parseFloat(position.offset) <= parseFloat(futureTransPercent));
+
+            for (let j = 0; j < filteredPoints.length; j++) {
+              const point = filteredPoints[j];
+              if (trigger.direction == 1 && point.isPast) continue;
+  
+              const section = point.element;
+              section.classList.remove("future", "pseudo-future");
+              pointPositions[pointPositions.indexOf(point)].isPast = true;
+            }
+          }
+
+          if (trigger.direction == -1) {
+            const filteredPoints = pointPositions.filter(position => parseFloat(position.offset) >= parseFloat(futureTransPercent));
+
+            for (let j = 0; j < filteredPoints.length; j++) {
+              const point = filteredPoints[j];
+              if (trigger.direction == -1 && !point.isPast) continue;
+  
+              const section = point.element;
+              section.classList.add("future", "pseudo-future");
+              pointPositions[pointPositions.indexOf(point)].isPast = false;
+            }
+          }
+        },
       }
     })
       .fromTo(document.documentElement,
@@ -73,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { [futureTransPercentVar]: futureTransPercent });
 
     const timelineStyle = document.head.appendChild(document.createElement("style"));
-    timelineStyle.innerHTML = `#skills .timeline:nth-of-type(${i + 1}):after { background: linear-gradient(180deg, rgb(var(--timeline-accent-rgb)) 0%, rgb(var(--timeline-accent-rgb)) calc(var(${futureTransPercentVar}) - ${20*pixelPercent}%), rgba(var(--foreground-rgb), 0.1) calc(var(${futureTransPercentVar}) - ${8*pixelPercent}%), rgba(var(--foreground-rgb), 0.1) 100%) !important; }`;
+    timelineStyle.innerHTML = `#skills .timeline:nth-of-type(${i + 1}):after { background: linear-gradient(180deg, rgb(var(--timeline-accent-rgb)) 0%, rgb(var(--timeline-accent-rgb)) calc(var(${futureTransPercentVar}) - ${20 * pixelPercent}%), rgba(var(--foreground-rgb), 0.1) calc(var(${futureTransPercentVar}) - ${8 * pixelPercent}%), rgba(var(--foreground-rgb), 0.1) 100%) !important; }`;
   }
 
   let changeMouseColor = gsap
