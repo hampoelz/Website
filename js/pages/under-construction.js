2afa2bfd733e5e0e8aa4f88e45c7a552dceddcfc
Rene Hampölz <rene_git@hampoelz.net>
2023-06-10 13:00:31 +0000

    chore: :children_crossing: Improve countdown effects

--- js/pages/under-construction.js
+++ js/pages/under-construction.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const countdown = document.querySelector('#under-construction .text .countdown');
+    const countdown_glitch = document.querySelector('#under-construction .text .glitch-container');
 
     const textScramble = new TextScramble(countdown, {
         classList: ['hover'],
@@ -17,40 +18,49 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
     let counter_fake_init = 10;
-    let counter_fake_error = -1;
+    let counter_fake_error = getRandomInt(-7, -4);
 
     let counter_fake = counter_fake_init;
-    let counter_redirect = Math.floor(Math.random() * 5);
-    console.log(counter_redirect);
+    let counter_redirect = getRandomInt(1 , 5);
 
     const count = async () => {
         if (counter_fake < counter_fake_error) {
             countdown.classList.remove("mark")
 
-            if (counter_fake == counter_fake_error - 1) {
-                await textScramble.setText('\x0B0 sec\x00',);
-            } else if (counter_fake <= counter_fake_error - 4) {
-                await textScramble.setText(`\x0B${counter_fake_init} secs\x00`);
-                counter_fake = counter_fake_init + 1;
-                counter_redirect--;
-            } else {
-                await textScramble.setText('\x0C\x0Berror\x00');
+            switch (counter_fake) {
+                case counter_fake_error - 1:
+                    await textScramble.setText('\x0C\x0Berrrr\x00');
+                    countdown_glitch.classList.remove("glitch");
+                    break;
+                case counter_fake_error - 2:
+                    await textScramble.setText('\x0C\x0Berror\x00');
+                    break;
+                default:
+                    await textScramble.setText(`\x0B${counter_fake_init} secs\x00`);
+                    counter_fake = counter_fake_init + 1;
+                    counter_redirect--;
             }
 
             setTimeout(count, 2000);
         } else {
             countdown.classList.add("mark")
 
-            if (counter_fake <= 1) {
+            if (counter_fake < -1) {
+                countdown_glitch.classList.add("glitch");
+                countdown.innerText = getRandomInt(-9, -1) + " sec";
+            } else if (counter_fake >= -1 && counter_fake <= 1) {
                 countdown.innerText = counter_fake + " sec";
             } else {
                 countdown.innerText = counter_fake + " secs";
             }
-            
-            console.log(counter_redirect);
-            if (counter_fake <= 0 && counter_redirect <= -1) {
-                window.location.href = "https://github.com/hampoelz";
+
+            countdown.dataset.text = countdown.innerText;
+
+            if (counter_fake <= 0 && counter_redirect <= 0) {
+                window.location.assign("https://github.com/hampoelz");
             } else {
                 setTimeout(count, 1000);
             }
